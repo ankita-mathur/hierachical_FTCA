75ce13518b0b176433afad3398ee6e37d6c0e13a
Ankita Mathur <anki.veela@gmail.com>
2012-01-11 12:56:11 +0530

    ed

--- src/com/gui/MainGUI.java
+++ src/com/gui/MainGUI.java
@@ -4,8 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainGUI extends JFrame {
 
@@ -35,8 +40,16 @@ public class MainGUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(4, 0, 0, 0));
+		
+		JButton btnGetData = new JButton("Get Data");
+		btnGetData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showInternalMessageDialog(contentPane, "Hello World");
+			}
+		});
+		contentPane.add(btnGetData);
 	}
 
 }
